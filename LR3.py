3067d21ee2f7967db168442ccb1933ba9fbdcaca
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-12-07 21:51:43 +0300

    Update LR3.py

--- LR3.py
+++ LR3.py
@@ -1,32 +1,66 @@
-#3
-from natasha import (
-    Segmenter,
-    NewsEmbedding,
-    NewsMorphTagger,
-    NewsSyntaxParser,
-    Doc
-)
+from natasha import Segmenter, NewsEmbedding, NewsMorphTagger, NewsSyntaxParser, Doc
 
-path = "C:/Users/Артём/source/repos/Jakepps/CompLeng/dataset.txt"
-from nltk import sent_tokenize
-
-file = open(path, "r", encoding="utf8")
-text = file.readlines()
-words = []
-file.close()
+def norm(txt):
+    _, x = map(int, txt.split('_'))
+    return x
 
 segmenter = Segmenter()
-emb = NewsEmbedding() #для кодировки слов
-morph_tagger = NewsMorphTagger(emb) #для морф разбора слов
-syntax_parser = NewsSyntaxParser(emb) #для синтаксического разбора слов
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
 
-for el in text:
-    sentences = sent_tokenize(el)
-    for i in range(len(sentences)):
-        sent = sentences[i]
-        doc = Doc(sent)
-        doc.segment(segmenter)
-        doc.tag_morph(morph_tagger)
-        doc.parse_syntax(syntax_parser)
-        print(doc.tokens[:5])
-        doc.sents[0].syntax.print()
+text = 'международное признание образовательных программ российских вузов'
+
+doc = Doc(text)
+doc.segment(segmenter)
+doc.tag_morph(morph_tagger)
+doc.parse_syntax(syntax_parser)
+
+sent = doc.sents[0]
+
+words = dict()
+for token in sent.tokens:
+    if token.pos != 'PUNCT':
+        norm_id = norm(token.id)
+        words[norm_id] = token.text
+        if token.rel == 'root':
+            root = norm_id
+tree = {0: []}
+for k in words.keys():
+    tree[k] = []
+for token in sent.tokens:
+    if token.pos != 'PUNCT':
+        norm_id = norm(token.id)
+        norm_head_id = norm(token.head_id)
+        tree[norm_head_id].append(norm_id)
+
+if 0 in tree.keys():
+    tree.pop(0)
+
+print('words:', words)
+print('root:', root)
+print('tree:', tree)
+
+# левое скобочное пpедставление деpева
+def lrep(a):
+    s = '('
+    s += words[a]
+    if len(tree[a]) > 0:
+        for t in tree[a]:
+            s += lrep(t)
+    s += ')'
+    return s
+
+# правое скобочное пpедставление деpева
+def rrep(a):
+    s = '('
+    # s += words[a] # отличие от ЛСПД
+    if len(tree[a]) > 0:
+        for t in tree[a]:
+            s += lrep(t)
+    s += words[a] # отличие от ЛСПД
+    s += ')'
+    return s
+
+sent.syntax.print() # для красоты
+print(rrep(root))
